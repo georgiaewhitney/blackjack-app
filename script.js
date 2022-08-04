1d2ba08d99b7e1d3b99dbe1b78ce61179f476963
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2022-08-04 18:31:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstCard = 9;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+console.log(messageEl);
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "Woohoo! You've got BlackJack!";
+    hasBlackJack = true;
+  } else { 
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
+
+
